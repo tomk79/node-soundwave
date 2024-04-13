43d43d915afe23e1dcdb24df59d00ccd57df557d
Tomoya Koyanagi <tomk79@gmail.com>
2024-04-13 22:11:29 +0900

    音量の取得ができるようになった。

--- src/main.mjs
+++ src/main.mjs
@@ -1,1 +1,70 @@
-export default class Soundwave {}
+export default class Soundwave {
+	#audioInput = null;
+	#audioInputType = null;
+	#options = {};
+	#events = {
+		sound: [],
+	};
+
+	constructor (audioInput, options) {
+		this.#audioInput = audioInput;
+		this.#options = options || {};
+		this.#audioInputType = (()=>{
+			if(audioInput instanceof HTMLElement){
+				return 'element';
+			}
+			if(audioInput instanceof MediaStream){
+				return 'stream';
+			}
+			return null;
+		})();
+
+		const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+		const sourceNode = (()=>{
+			if(this.#audioInputType == 'element'){
+				return audioContext.createMediaElementSource(audioInput);
+			}
+			if(this.#audioInputType == 'stream'){
+				return audioContext.createMediaStreamSource(audioInput);
+			}
+		})();
+
+		const analyserNode = audioContext.createAnalyser();
+		analyserNode.fftSize = 256;
+
+		sourceNode.connect(analyserNode);
+		if(this.#options.destination){		
+			analyserNode.connect(audioContext.destination);
+		}
+
+		const dataArray = new Uint8Array(analyserNode.frequencyBinCount);
+
+		const getVolumeLevel = () => {
+			analyserNode.getByteFrequencyData(dataArray);
+
+			let sum = dataArray.reduce((a, b) => a + b, 0);
+			let average = sum / dataArray.length;
+			let per = Number(average/255) * 2;
+
+			return per;
+		};
+
+		const updateVolumeLevel = () => {
+			const volumeLevel = getVolumeLevel();
+
+			this.#events['sound'].forEach((callback)=>{
+				callback(volumeLevel);
+			});
+			requestAnimationFrame(updateVolumeLevel);
+		};
+		updateVolumeLevel();
+	}
+
+	on (eventName, callback) {
+		this.#events[eventName].push(callback);
+	}
+
+	off (eventName) {
+		this.#events[eventName] = [];
+	}
+}
--- tests/app/src/contents.js
+++ tests/app/src/contents.js
@@ -1,4 +1,39 @@
 (function(){
-    const soundWave = new window.SoundWave();
-    console.log(soundWave);
+
+	// --------------------------------------
+	// 音声ファイルの音量を取得する
+	(() => {
+		const audioElement = new Audio();
+
+		const volumeLevelDisplay = $('#volume-indicator__audio-element');
+		const soundWave = new window.SoundWave(audioElement, {destination: true});
+
+		soundWave.on('sound', function(volume){
+			let volumePer = (volume * 100) + '%';
+			if(audioElement.ended){
+				volumePer = 0 + '%';
+			}
+			volumeLevelDisplay.text(`Level: ${volumePer}`);
+			volumeLevelDisplay.css({width: volumePer});
+		});
+
+		audioElement.src = "./sounds/sample.m4a";
+		audioElement.controls = true;
+		$('#audio-container').append(audioElement);
+	})();
+
+
+	// --------------------------------------
+	// マイクの音量を取得する
+	navigator.mediaDevices.getUserMedia({ audio: true })
+		.then((stream)=>{
+			const volumeLevelDisplay = $('#volume-indicator__user-media');
+			const soundWave = new window.SoundWave(stream, {destination: true});
+			soundWave.on('sound', function(volume){
+				let volumePer = (volume * 100) + '%';
+				volumeLevelDisplay.text(`Level: ${volumePer}`);
+				volumeLevelDisplay.css({width: volumePer});
+			});
+		});
+
 })();
